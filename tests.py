525bbb415122e854ddb138a775d7d18bb60349e6
Leo <leoch2340@gmail.com>
2024-10-04 18:45:04 +0300

    Добавляем бибилиотеки

---
+++ tests.py
@@ -0,0 +1,6 @@
+import unittest  # Импортирует модуль unittest для создания и выполнения тестов
+import os  # Импортирует модуль os для взаимодействия с операционной системой
+import tempfile  # Импортирует модуль tempfile для работы с временными файлами и директориями
+from emulator import execute_command  # Импортирует функцию execute_command из модуля emulator
+import shutil  # Импортирует модуль shutil для работы с файлами и директориями
+
